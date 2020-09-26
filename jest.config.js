d63c82108134785c15f7ceeba6b177b8554a0039
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2020-09-26 12:19:46 -0700

    build: configure jest and storyshots

---
+++ jest.config.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const CI = !!process.env.CI;
+
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'reports/coverage',
+  coverageProvider: 'v8',
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/file-mock.js',
+  },
+  reporters: [
+    'default',
+    CI && [
+      'jest-junit',
+      {
+        ancestorSeparator: ' › ',
+        classNameTemplate: '{classname}',
+        includeConsoleOutput: true,
+        outputDirectory: 'reports/junit',
+        outputName: `jest${
+          process.env.BUILDKITE_JOB_ID ? `+${process.env.BUILDKITE_JOB_ID}` : ''
+        }.xml`,
+        suiteName: 'Unit Tests',
+        titleTemplate: '{title}',
+      },
+    ],
+  ].filter(Boolean),
+  testEnvironment: 'jsdom',
+  testMatch: [
+    '**/src/**/__tests__/**/*.[jt]s?(x)',
+    '**/src/**/?(*.)+(spec|test).[tj]s?(x)',
+  ],
+  testPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '^.+\\.stories\\.[tj]sx?$': '@storybook/addon-storyshots/injectFileName',
+    // babel-jest is loaded automatically when transform is null
+    // eslint-disable-next-line sort-keys
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+};
