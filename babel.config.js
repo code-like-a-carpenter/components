681745fd2d3621e62c6fb4f36c6d58ebb097b05c
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2020-09-26 09:21:18 -0700

    build: configure babel

---
+++ babel.config.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = function (api) {
+  api.cache.using(
+    () =>
+      `${process.env.BABEL_ENV}${process.env.NODE_ENV}${process.env.BUILD_TARGET}`
+  );
+
+  const config = {
+    comments: true,
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-optional-chaining',
+    ],
+    presets: [
+      '@babel/preset-typescript',
+      [
+        '@babel/preset-react',
+        {
+          development:
+            process.env.BABEL_ENV === 'development' ||
+            process.env.NODE_ENV === 'development',
+        },
+      ],
+      [
+        '@babel/preset-env',
+        {
+          modules: process.env.BUILD_TARGET === 'modules' ? false : undefined,
+          targets: process.env.BUILD_TARGET
+            ? 'last 2 versions, > 5%, not dead'
+            : {node: true},
+        },
+      ],
+    ],
+    retainLines: process.env.NODE_ENV !== 'production',
+    sourceMaps: true,
+  };
+
+  return config;
+};
