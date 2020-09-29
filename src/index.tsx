68e1c2d116cb226619a6c85117d0cb02400ac0c0
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2020-09-28 21:06:36 -0700

    feat: migrate Markdown from check-run-reporter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 export * from './alert';
 export * from './code';
 export * from './link';
+export * from './markdown';
 export * from './renderers';
 export * from './outline';
 export * from './spinner';
