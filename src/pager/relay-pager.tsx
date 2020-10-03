9b8cdc1f563f725de8573b2803180b40c7edd681
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2020-10-03 16:18:15 -0700

    feat(pages): add RelayPager

---
+++ src/pager/relay-pager.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Pagination} from 'react-bootstrap';
+
+import {useRoutingContext} from '..';
+
+import {makeRelayHref} from './hrefs';
+import {RelayPageInfo} from './types';
+
+export type RelayPagerProps = {
+  pageInfo: RelayPageInfo;
+};
+
+export const RelayPager = ({pageInfo}: RelayPagerProps) => {
+  const routingContext = useRoutingContext();
+
+  return (
+    <Pagination>
+      <Pagination.Prev
+        disabled={!pageInfo.hasPreviousPage}
+        href={makeRelayHref(routingContext, pageInfo, 'previous')}
+      />
+      <Pagination.Next
+        disabled={!pageInfo.hasNextPage}
+        href={makeRelayHref(routingContext, pageInfo, 'next')}
+      />
+    </Pagination>
+  );
+};
