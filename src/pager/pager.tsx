05baf4db03b80e154f586f093ad5199732476182
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2020-10-03 16:25:23 -0700

    feat(pager): add all-purpose Pager

---
+++ src/pager/pager.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {OffsetPager} from './offset-pager';
+import {RelayPager} from './relay-pager';
+import {SimplePager} from './simple-pager';
+import {PageInfo} from './types';
+
+export type PagerProps = {
+  pageInfo: PageInfo;
+};
+
+export const Pager = ({pageInfo}: PagerProps) => {
+  if ('skip' in pageInfo) {
+    return <OffsetPager pageInfo={pageInfo} />;
+  }
+
+  if ('page' in pageInfo) {
+    return <SimplePager pageInfo={pageInfo} />;
+  }
+
+  return <RelayPager pageInfo={pageInfo} />;
+};
