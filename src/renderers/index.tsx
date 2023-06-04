ace080c8080bb6f3dfaf66b06872ee4ae24f924e
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2023-06-04 15:39:51 -0400

    feat: document how to pass props to renderers without binding a new function

--- src/renderers/index.tsx
+++ src/renderers/index.tsx
@@ -2,6 +2,7 @@ export * from './any-renderer';
 export * from './boolean-renderer';
 export * from './date-renderer';
 export * from './maybe-renderer';
-export * from './object-renderer';
 export * from './null-renderer';
+export * from './object-renderer';
+export * from './render';
 export * from './types';
