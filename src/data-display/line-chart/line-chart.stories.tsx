c194fb44157d813816db1a36b92e16a3241b4571
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2023-06-13 22:49:26 -0400

    refactor(line-chart): replace visx with recharts

--- src/data-display/line-chart/line-chart.stories.tsx
+++ src/data-display/line-chart/line-chart.stories.tsx
@@ -1,73 +1,76 @@
+import type {Meta, StoryObj} from '@storybook/react';
 import {appleStock, cityTemperature} from '@visx/mock-data';
 
 import {LineChart} from './line-chart';
 
-export default {
+const meta: Meta<typeof LineChart> = {
   component: LineChart,
+  decorators: [
+    (Story) => (
+      <div style={{height: '30vh', width: '60vw'}}>
+        <Story />
+      </div>
+    ),
+  ],
   title: 'Data Display/Line Chart',
 };
 
-export const SingleSeries = () => (
-  <>
-    <LineChart
-      yMin={0}
-      series={[
-        {
-          data: appleStock.slice(0, 100).map((as) => ({
-            x: new Date(as.date),
-            y: as.close,
-          })),
-          label: 'Apple Stock',
-        },
-      ]}
-    />
-  </>
-);
+export default meta;
 
-export const LowDensityData = () => (
-  <>
-    <LineChart
-      yMin={0}
-      series={[
-        {
-          data: appleStock.slice(0, 10).map((as) => ({
-            x: new Date(as.date),
-            y: as.close,
-          })),
-          label: 'Apple Stock',
-        },
-      ]}
-    />
-  </>
-);
+type Story = StoryObj<typeof LineChart>;
 
-export const MultipleSeries = () => (
-  <>
-    <LineChart
-      yMin={0}
-      series={[
-        {
-          data: cityTemperature.slice(0, 100).map((ct) => ({
-            x: new Date(ct.date),
-            y: Number(ct.Austin),
-          })),
-          label: 'Austin',
-        },
-        {
-          data: cityTemperature.slice(0, 100).map((ct) => ({
-            x: new Date(ct.date),
-            y: Number(ct['New York']),
-          })),
-          label: 'New York',
-        },
-        {
-          data: cityTemperature.slice(0, 100).map((ct) => ({
-            x: new Date(ct.date),
-            y: Number(ct['San Francisco']),
-          })),
-          label: 'San Francisco',
-        },
-      ]}
-    />
-  </>
-);
+export const SingleSeries: Story = {
+  args: {
+    series: [
+      {
+        data: appleStock.slice(0, 100).map((as) => ({
+          x: new Date(as.date),
+          y: as.close,
+        })),
+        label: 'Apple Stock',
+      },
+    ],
+  },
+};
+
+export const LowDensityData: Story = {
+  args: {
+    series: [
+      {
+        data: appleStock.slice(0, 10).map((as) => ({
+          x: new Date(as.date),
+          y: as.close,
+        })),
+        label: 'Apple Stock',
+      },
+    ],
+  },
+};
+
+export const MultipleSeries: Story = {
+  args: {
+    series: [
+      {
+        data: cityTemperature.slice(0, 100).map((ct) => ({
+          x: new Date(ct.date),
+          y: Number(ct.Austin),
+        })),
+        label: 'Austin',
+      },
+      {
+        data: cityTemperature.slice(0, 100).map((ct) => ({
+          x: new Date(ct.date),
+          y: Number(ct['New York']),
+        })),
+        label: 'New York',
+      },
+      {
+        data: cityTemperature.slice(0, 100).map((ct) => ({
+          x: new Date(ct.date),
+          y: Number(ct['San Francisco']),
+        })),
+        label: 'San Francisco',
+      },
+    ],
+  },
+};
